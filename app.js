334efb085d22f99fb486c8d0510a66b34ebdc585
Ceina Ellison <ellisoce@classwork.engr.oregonstate.edu>
2026-05-07 13:36:13 -0700

    set up project

---
+++ app.js
@@ -0,0 +1,44 @@
+/*
+    SETUP
+*/
+
+// Express
+const express = require('express');  // We are using the express library for the web server
+const app = express();               // We need to instantiate an express object to interact with the server in our code
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+const PORT = 5386;    
+
+// Database 
+const db = require('./db-connector');
+
+// Handlebars
+const { engine } = require('express-handlebars'); // Import express-handlebars engine
+app.engine('.hbs', engine({ extname: '.hbs' })); // Create instance of handlebars
+app.set('view engine', '.hbs'); // Use handlebars engine for *.hbs files.
+
+/*
+    ROUTES
+*/
+
+app.get('/', async function (req, res) {
+    try {
+        res.render('home'); // Render the home.hbs file
+    } catch (error) {
+        console.error('Error rendering page:', error);
+        // Send a generic error message to the browser
+        res.status(500).send('An error occurred while rendering the page.');
+    }
+});
+
+
+
+/*
+    LISTENER
+*/
+
+app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
+    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+});
