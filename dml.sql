ee4903d000d6e30ee326200aea26386f6c064521
Eman Alturky <alturkye@oregonstate.edu>
2026-05-07 17:17:24 -0700

    Initial push of dml.sql to new repo

---
+++ dml.sql
@@ -0,0 +1,125 @@
+-- Group 2: Ceina Ellison, Eman Alturky
+
+-----
+-- MEMBERS PAGE
+-----
+
+-- SELECT: get all member detail to display in the browse table
+SELECT * FROM Members;
+
+-- INSERT: add a new member using variables from the web form
+INSERT INTO Members (first_name, last_name, email, phone_number, membership_start_date, trainer_id)
+VALUES (:first_name_input, :last_name_input, :email_input, :phone_input, :start_date_input, :trainer_id_from_dropdown);
+
+-- UPDATE: modify a member's information based on the ID selected from the table
+UPDATE Members
+SET first_name = :first_name_input,
+    last_name = :last_name_input,
+    email = :email_input,
+    phone_number = :phone_input,
+    membership_start_date = :start_date_input,
+    trainer_id = :trainer_id_from_dropdown
+WHERE member_id = :member_id_selected_from_table;
+
+-- DELETE: remove a member from the database
+DELETE FROM Members WHERE member_id = :member_id_selected_from_table;
+
+------
+-- TRAINERS PAGE
+------
+-- SELECT: get all trainers to display in the browse table
+SELECT * FROM Trainers;
+
+-- INSERT: add a new trainer
+INSERT INTO Trainers (first_name, last_name, specialization, hourly_rate)
+VALUES (:first_name_input, :last_name_input, :specialization_input, :hourly_rate_input);
+
+-- UPDATE: edit trainer details
+UPDATE Trainers
+SET first_name = :first_name_input,
+    last_name = :last_name_input,
+    specialization = :specialization_input,
+    hourly_rate = :hourly_rate_input
+WHERE trainer_id = :trainer_id_selected_from_table;
+
+-- DELETE: remove a trainer
+DELETE FROM Trainers WHERE trainer_id = :trainer_id_selected_from_table;
+
+-----
+-- CLASSES PAGE
+-----
+
+-- SELECT: get all classes to browse
+SELECT * FROM Classes;
+
+-- INSERT: add a new fitness class
+INSERT INTO Classes (class_name, description, max_capacity, trainer_id, room_location)
+VALUES (:class_name_input, :description_input, :max_capacity_input, :trainer_id_from_dropdown, :room_location_input);
+
+-- UPDATE: edit class details
+UPDATE Classes
+SET class_name = :class_name_input,
+    description = :description_input,
+    max_capacity = :max_capacity_input,
+    trainer_id = :trainer_id_from_dropdown,
+    room_location = :room_location_input
+WHERE class_id = :class_id_selected;
+
+-- DELETE: cancel a class
+DELETE FROM Classes WHERE class_id = :class_id_selected;
+
+-----
+-- EQUIPMENT PAGE
+-----
+
+-- SELECT: get all equipment records for the browse table
+SELECT * FROM Equipment_Records;
+
+-- CREATE: log a new piece of equipment
+INSERT INTO Equipment_Records (item_name, maintenance_status, purchase_date, location)
+VALUES (:item_name_input, :maintenance_status_input, :purchase_date_input, :location_input);
+
+-- UPDATE: edit equipment details
+UPDATE Equipment_Records
+SET item_name = :item_name_input,
+    maintenance_status = :maintenance_status_input,
+    purchase_date = :purchase_date_input,
+    location = :location_input
+WHERE equipment_id = :equipment_id_selected_from_table;
+
+-- DELETE: remove an equipment record
+DELETE FROM Equipment_Records WHERE equipment_id = :equipment_id_selected_from_table;
+
+-----
+-- ENROLLMENTS PAGE
+-----
+-- SELECT: view which members are in which classes
+SELECT Enrollments.enrollment_id, Members.first_name, Members.last_name, Classes.class_name, Enrollments.signup_date
+FROM Enrollments
+JOIN Members ON Enrollments.member_id = Members.member_id
+JOIN Classes ON Enrollments.class_id = Classes.class_id;
+
+-- CREATE: enroll a member in a class
+INSERT INTO Enrollments (member_id, class_id, signup_date)
+VALUES (:member_id_from_dropdown, :class_id_from_dropdown, :signup_date_input);
+
+-- DELETE: remove a member's enrollment
+DELETE FROM Enrollments WHERE enrollment_id = :enrollment_id_selected;
+
+-----
+-- CLASSES_EQUIPMENT PAGE
+-----
+
+-- SELECT: list which equipment is assigned to which class
+SELECT Classes.class_name, Equipment_Records.item_name
+FROM Classes_Equipment
+JOIN Classes ON Classes_Equipment.class_id = Classes.class_id
+JOIN Equipment_Records ON Classes_Equipment.equipment_id = Equipment_Records.equipment_id;
+
+-- CREATE: assign equipment to a class
+INSERT INTO Classes_Equipment (class_id, equipment_id)
+VALUES (:class_id_from_dropdown, :equipment_id_from_dropdown);
+
+-- DELETE: remove an equipment assignment from a class
+DELETE FROM Classes_Equipment
+WHERE class_id = :class_id_from_dropdown AND equipment_id = :equipment_id_from_dropdown;
